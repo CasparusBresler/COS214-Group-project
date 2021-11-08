984a4064bddd49001ead3f8dfe4967a3c332073c
CasparusBresler <u20780479@tuks.co.za>
2021-11-08 21:35:55 +0200

    FalconRocketPattern
    Most of the falcon rocket pattern. FalconHeavy not complete.

---
+++ FalconHeavy.cpp
@@ -0,0 +1,37 @@
+// Casparus Bresler
+
+#include "FalconHeavy.h"
+
+FalconHeavy::FalconHeavy()
+{
+}
+
+FalconHeavy::~FalconHeavy()
+{
+}
+
+void FalconHeavy::launch_sequence()
+{
+    //TODO launch rocket
+
+}
+
+void FalconHeavy::fire1()
+{
+    //TODO fire booster 1
+}
+
+void FalconHeavy::fire2()
+{
+    //TODO fire booster 2
+}
+
+void FalconHeavy::load_sat(satellite *s)
+{
+    
+}
+
+void FalconHeavy::deploy()
+{
+    //TODO deploy satellite (i think?)
+}
