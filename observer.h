33ff487bcaa36fef1ccaaf45fe1fe61536bcccae
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:43:28 +0200

    Update observer.h

--- observer.h
+++ observer.h
@@ -9,13 +9,24 @@
 
 class Mediator;
 
+/// This is the observer class.
+///
+/// It has an object of type Mediator as a private member
 class observer
 {
 private:
     Mediator *med;
 
 public:
+    /// This is the observer Constructor.
+/// 
+///@param m is an object of type Mediator
     observer(Mediator *);
+    /// This is the update function.
+/// 
+///This used to update the system_online of the satellite
+/// @param tf a bool value passed in.
+/// @return nothing
     void update(bool);
 };
 
