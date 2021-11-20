092a3fd51e3de4570a626407b040a39b856a5387
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:37:16 +0200

    Update satellite_factory.h

--- satellite_factory.h
+++ satellite_factory.h
@@ -10,14 +10,25 @@
 #include "Mediator.h"
 #include "satellite.h"
 
+/// This is the factory class for satellites.
+///
+/// It has as an integer counter, an object of type aggregate and an object of type Mediator as a private member
 class satellite_factory
 {
 private:
+    
     aggregate* agg;
     Mediator* med;
     int counter;
 public:
+    ///This is the satellite_factory constructor.
+///
+///@param a is an object of type aggregate
+///@param m is an object of type Mediator
     satellite_factory(aggregate*, Mediator*);
+    ///This is the create_sat function.
+///
+///@return an object of type satellite
     satellite* create_sat();
     void setcounter(int);
 
