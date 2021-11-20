7ad2e48a8ff4645eada7f89257e61c100a3136e0
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:32:52 +0200

    Update aggregate.h

--- aggregate.h
+++ aggregate.h
@@ -5,8 +5,14 @@
 #ifndef AGGREGATE_H
 #define AGGREGATE_H
 
-
 #include "Iterator.h"
+
+using namespace std;
+/**
+ * @brief Define new class aggregate 
+ * Uses the Iterator Design pattern
+ * contains a vector of satellites which is defined privately
+ */
 
 class aggregate
 {
@@ -14,9 +20,29 @@ private:
     vector<satellite *> satellites;
 
 public:
+     /**
+     * @brief Construct a new aggregate object
+     * Constructor for aggregate class
+     * @return nothing
+     * 
+     */
     aggregate();
-    Iterator *createIterator();
-    void addSat(satellite *);
+    
+      /**
+     * @brief Create an Iterator object
+     * Uses the Iterator design pattern as well as the Factory Method design pattern to create a new object
+     * @return Iterator* 
+     */
+    Iterator* createIterator();
+    
+      /**
+     * @brief addSat function of aggregate class
+     * Takes in parameter s and adds a new satellite to the program
+     * @return nothing
+     * @param s 
+     */
+    void addSat(satellite* s);
+    
     aggregate *clone();
 };
 
