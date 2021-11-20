3fa0d56a7bc7446a9bd0f0b386b98913097b8c84
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:41:13 +0200

    Update docked_state.h

--- docked_state.h
+++ docked_state.h
@@ -5,15 +5,57 @@
 
 using namespace std;
 
+/**
+ * @brief new class docked_state
+ * Uses the State design pattern
+ */
+
 class docked_state
 {
 private:
 
 public:
+       /**
+     * @brief Construct a new docked_state object
+     * Constructor for the docked_state class
+     * @return nothing
+     */
     docked_state(/* args */);
+    
+     /**
+     * @brief Destroy the docked_state object
+     * Deconstructor for the docked_state class
+     * @return nothing
+     */
     ~docked_state();
+    
+     /**
+     * @brief virtual getstate function for the docked_state class
+     * Implemented in subclasses
+     * Uses the State design pattern
+     * @see docked class
+     * @return true 
+     * @return false 
+     */
     virtual bool getstate() = 0;
+
+     /**
+     * @brief virtual changestate function for the docked_state class
+     * Implemented in subclasses
+     * Uses the State design pattern
+     * @see docked class
+     * @return docked_state* 
+     */
     virtual docked_state *changestate() = 0;
+    
+     /**
+     * @brief virtual clone function for the docked_state class
+     * Implemented in subclasses
+     * Uses the Prototype design pattern as well as the State design pattern
+     * Used to clone a new docked_state object in its subclasses
+     * 
+     * @return docked_state* 
+     */
     virtual docked_state* clone() = 0;
 };
 #endif // __DOCKED_STATE_H__
