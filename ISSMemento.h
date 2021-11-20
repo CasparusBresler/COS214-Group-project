3ea9486a13780696fec01d9d01963b14a74769aa
CasparusBresler <92540552+CasparusBresler@users.noreply.github.com>
2021-11-20 21:53:42 +0200

    Update ISSMemento.h

--- ISSMemento.h
+++ ISSMemento.h
@@ -9,7 +9,9 @@
 #include "vector"
 
 using namespace std;
-
+/// This class is the memento the international space station 
+///
+///It has a vector of object type crew, vector of object type cargo and  an object of type docked_state which are all private.
 class ISSMemento
 {
 private:
@@ -18,10 +20,33 @@ private:
     docked_state* state;
 
 public:
+    
+/// This is the ISSMemento Constructor.
+/// 
+///
     ISSMemento(vector<crew*> crewV, vector<cargo*> cargoV, docked_state* ds);
+    
+/// This is the ISSMemento Deconstructor.
+/// 
+///
     ~ISSMemento();
+    
+/// This is the getCrew funcion.
+///
+/// This used as an accessor method to private member crewmembers
+/// @return vector of object type crew 
     vector<string> getcrew();
+    
+/// This is the getCargo funcion.
+///
+/// This used as an accessor method to private member cargohold
+/// @return vector of object type cargo 
     vector<string> getcargo();
+    
+/// This is the getDockedState funcion.
+///
+/// This used as an accessor method to private member crewmembers
+/// @return an object of type docked_state
     docked_state* getState();
 };
 #endif // __ISSMEMENTO_H__
