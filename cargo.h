040f63f20204f460b2e8e4c920c9e03e2b3fb19c
CasparusBresler <92540552+CasparusBresler@users.noreply.github.com>
2021-11-20 21:33:00 +0200

    Update cargo.h

--- cargo.h
+++ cargo.h
@@ -9,16 +9,54 @@
 
 using namespace std;
 
+/**
+ * @brief new class cargo, inherited from storage class
+ * Takes in a string which is declared privately
+ * @see crew class
+ */
 class cargo : public storage
 {
 private:
 
     string name;
 public:
+    /**
+     * @brief Construct a new cargo object
+     * Constructor for cargo class
+     * Initializes string name which is declared privately
+     * @return nothing
+     * @param s 
+     */
     cargo(string s);
+    
+    /**
+     * @brief Destroy the cargo object
+     * Destructor for cargo class
+     * @return nothing
+     */
     ~cargo();
+    
+    /**
+     * @brief Get the Name object
+     * Function to get and return the name of the cargo being stored
+     * @return string 
+     */
     string getName();
+    
+    /**
+     * @brief Set the Name object
+     * Sets the name of the cargo being stored 
+     * @return nothing
+     * @param word 
+     */
     void setName(string word);
+    
+    /**
+     * @brief clone function of cargo class
+     * Uses the Prototype design pattern
+     * @see crew* clone() - in crew class
+     * @return cargo* 
+     */
     cargo* clone();
 };
 
