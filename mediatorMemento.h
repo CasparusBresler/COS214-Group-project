362a4b8c5fe316ae05f3aba1a1a376580e57db65
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:46:47 +0200

    Update mediatorMemento.h

--- mediatorMemento.h
+++ mediatorMemento.h
@@ -9,8 +9,16 @@
 #include "aggregate.h"
 #include <string>
 
+
+///Forward class declaration
+///
+///
 class observer;
 
+
+///This is the mediatorMemento class
+///
+/// It has as an object of type aggregate and an object of type Iterator as private memebers
 class mediatorMemento
 {
 private:
@@ -22,8 +30,20 @@ private:
     // Iterator *it;
 
 public:
+    /// This is the ISS Constructor.
+/// 
+///@param i is as an object of type aggregate
+///@param o is an object of type Iterator
     mediatorMemento(int s, vector<string> n, vector<bool> o, vector<bool> OS);
+    /// This is the getSatellites funcion.
+///
+/// This used as an accessor method to private member satellites
+/// @return vector of object type aggregate 
     int getSize();
+    /// This is the getIT funcion.
+///
+/// This used as an accessor method to private member it
+/// @return vector of object type Iterator 
     vector<string> getNames();
     vector<bool> getOrbits();
     vector<bool> getOStatus();
