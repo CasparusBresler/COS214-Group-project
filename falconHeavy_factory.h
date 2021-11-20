c14bb1de1d4bcce742ba5587b19cd49934bb9d74
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:52:01 +0200

    Update falconHeavy_factory.h

--- falconHeavy_factory.h
+++ falconHeavy_factory.h
@@ -8,12 +8,27 @@
 
 using namespace std;
 
+/// Rocket factory of type FalconHeavy.
+
 class falconHeavy_factory : public falcon_factory
 {
 private:
 public:
+    /// This is the falconHeavy_factory Constructor.
+    ///
+    ///
     falconHeavy_factory();
+    
+    /// This is the falconHeavy_factory deconstructor
+    /// 
+    ///
     ~falconHeavy_factory();
+    
+    // This is the createFalcon function
+    /// 
+    /// This is used to as the the part of the abstract Factory design pattern.
+    ///@param fsr is an object of type FirstStageRocket
+    /// @return object of type strategy
     strategy *createFalcon(FirstStageRocket* fsr);
 };
 
