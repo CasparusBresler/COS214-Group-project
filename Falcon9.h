d0686984be1c6c00b4562a53b2444596e4b9b0ea
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:30:09 +0200

    Update Falcon9.h

--- Falcon9.h
+++ Falcon9.h
@@ -8,6 +8,10 @@
 
 using namespace std;
 
+/// Falcon Rocket of type Falcon9 .
+///
+/// Contains a vector of satellites and a FirstStageRocket object which are both private.
+
 class Falcon9 : public strategy
 {
 private:
@@ -15,12 +19,55 @@ private:
     vector<satellite *> satellites;
     FirstStageRocket* firstStageRocket;
 public:
+
+    /// This is the Falcon9 Constructor.
+    /// 
+    ///@param fsr is an object of type FirstStageRocket
     Falcon9(FirstStageRocket* fsr);
+    
+    /// This is the Falcon9 deconstructor
     ~Falcon9();
+    
+    /// This is the launch_sequence function
+    /// 
+    /// This is used to as the the part of the strategy design pattern.
+    /// This is responsible for the launch of the Falcon9.
+    /// @return nothing
+    /// @see fire_booster1() fire_booster2() deploy()
     void launch_sequence();
+    
+    
+    /// This is the fire_booster1 function
+    /// 
+    /// This is responsible for the first stage of the launch.
+    /// @return nothing
+    /// @see launch_sequence()
     void fire_booster1();
+    
+    
+    /// This is the fire_booster2 function
+    /// 
+    /// 
+    /// This is responsible for the second stage of the launch. Executes after the first stage
+    /// @return nothing
+    /// @see launch_sequence()
     void fire_booster2();
+    
+    /// This is the load_sat function
+    /// 
+    /// 
+    /// This is responsible for loading the satellite.
+    ///@param s is an object of type satellite
+    /// @return nothing
     void load_sat(satellite *s);
+   
+    
+    /// This is the deploy function
+    /// 
+    /// 
+    /// This is responsible for deploying the satellites once the rockets are in orbit. Executes after the second stage
+    /// @return nothing
+    /// @see launch_sequence()
     void deploy();
 };
 
