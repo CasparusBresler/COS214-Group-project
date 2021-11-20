023c6e1ec672afbde9e9b6e9749060d27fd610c0
CasparusBresler <92540552+CasparusBresler@users.noreply.github.com>
2021-11-20 21:39:08 +0200

    Update deploy_sats.h

--- deploy_sats.h
+++ deploy_sats.h
@@ -2,12 +2,37 @@
 #define __DEPLOY_SATS_H__
 
 #include "falcon_command.h"
+
+/**
+ * @brief new deploy_sats class, inherited from the falcon_command class
+ * 
+ */
 class deploy_sats : public falcon_command
 {
 private:
 public:
+
+    /**
+     * @brief Construct a new deploy sats object
+     * Constructor for the deploy_sats class
+     * @return nothing
+     */
     deploy_sats();
+    
+    /**
+     * @brief Destroy the deploy sats object
+     * Deconstructor for the deploy_sats class
+     * @return nothing
+     */
     ~deploy_sats();
+    
+    /**
+     * @brief execute function for deploy_sats class
+     * 
+     * @see falcon_command class
+     * @param r 
+     * @param s 
+     */
     void execute(strategy *r, satellite *s);
 };
 
