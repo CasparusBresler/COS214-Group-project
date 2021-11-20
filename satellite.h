c16a18f483642acb7ccfb6a279bbb04faef394b5
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:40:29 +0200

    Update satellite.h

--- satellite.h
+++ satellite.h
@@ -13,6 +13,9 @@ class Mediator;
 
 using namespace std;
 
+///This is the satellite class.
+///
+///It has a string name, bool inorbit, an object of type observer and a bool System_online as a private member
 class satellite
 {
 private:
@@ -22,16 +25,65 @@ private:
     bool System_online;
 
 public:
+    /// this is the getName function.
+///
+/// This used as an accessor method to private member name
+/// @return a string containing the name of the satellite
     string getName();
+    /// this is the setOrbit function.
+///
+/// This used as an modifier method to private member bool
+///@param tf the bool value that is going to be passed in.
+/// @return nothing
     void setOrbit(bool);
+    /// this is the getOrbit function.
+///
+/// This used as an accessor method to private member inorbit
+/// @return a bool value 
     bool getOrbit();
+    /// this is the attach function.
+///
+///This used to attach the satellite
+///@param o is an object of type observer
+/// @return nothing
     void attach(observer*);
+    /// this is the detach function.
+///
+/// This used to detach the satellite
+/// @return nothing
     void detach();
+    /// this is the setSO function.
+///
+/// This used as an modifier method to private member System_online
+///@param tf the bool value that is going to be passed in.
+/// @return nothing
     void setSO(bool);
+    /// this is the getS0 function.
+///
+/// This used as an accessor method to private member System_online
+/// @return a bool value 
     bool getS0();
+    /// this is the setSOandUpdate function.
+///
+/// This used as an accessor method to private member System_online and updates observers
+///@param tf the bool value that is going to be passed in.
+/// @return nothing
     void setSOandUpdate(bool);
+    /// this is the notify function.
+///
+/// This used to notify observers
+/// @return nothing
     void notify();
+    /// this is the getObserv function.
+///
+/// This used as an accessor method to private member observ
+///@return an object of type observer
     observer* getObserv();
+    /// this is the satellite constructor.
+///
+///@param n is for the a string name
+///@param med is an object of type observer
+///@return a string containing the name of the satellite
     satellite(string, Mediator*);
     void workLoad(bool);
     void setName(string s);
