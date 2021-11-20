1e36d2427e3003976eb8dde2d0a269fc93c0e826
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:50:33 +0200

    Update load.h

--- load.h
+++ load.h
@@ -5,13 +5,28 @@
 
 using namespace std;
 
+///This class is responsible for the loading storage on to the rockets of type dragon
+///
+///
 class load : public DragonCommand
 {
 private:
 public:
 
+/// This is the load Constructor.
+/// 
+///
     load();
+    /// This is the load deconstructor.
+/// 
+///
     ~load();
+    /// This is the execute function.
+/// 
+/// This is used to excute a command and is a part of the command design pattern
+///@param r is an object of type dragon_template
+///@param s is an object of type storage
+///@return Nothing
     void execute(dragon_template* , storage*) override;
 
 };
