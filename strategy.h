8cd936d63ee6c457309749e20081495e6658fbd8
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:31:05 +0200

    Update strategy.h

--- strategy.h
+++ strategy.h
@@ -15,12 +15,37 @@ private:
     bool launched;
 
 public:
+    /// This is the strategy Constructor.
+    ///
+    ///
     strategy();
+    /// This is the strategy deconstructor.
+    ///
+    /// 
     ~strategy();
+    /// This is the launch_sequence function
+     ///
+     /// This is pure virtual function which that will be implemented by the concrete classes
     virtual void launch_sequence() = 0;
+     /// This is the load_sat function
+     ///
+     ///@param s is an object of type satellite
+     /// This is pure virtual function which that will be implemented by the concrete classes
     virtual void load_sat(satellite *s) = 0;
+     /// This is the deploy function
+     ///
+     /// This is pure virtual function which that will be implemented by the concrete classes
     virtual void deploy() = 0;
+    /// this is the getLaunched function.
+    ///
+    /// This used as an accessor method to private member launched
+    /// @return a bool value 
     bool getLaunched();
+    /// this is the setLaunched function.
+    ///
+    /// This used as an modifier method to private member System_online
+    ///@param lIn the bool value that is going to be passed in.
+    /// @return nothing
     void setLaunched(bool lIn);
 };
 
