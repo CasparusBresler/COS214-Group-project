d4339a5b1c0ebc7b29825b5dca33d6b8767c1677
MichaelViljoen <92848541+MichaelViljoen@users.noreply.github.com>
2021-11-20 21:28:51 +0200

    Update undocked.h

--- undocked.h
+++ undocked.h
@@ -9,15 +9,36 @@
 
 using namespace std;
 
+///This is the undocked class
+///
+///It has a bool attribute as a private member
 class undocked : public docked_state
 {
 private:
     bool dock_state;
 public:
+    /// This is the undocked Constructor.
+    ///
+    ///
     undocked(/* args */);
+    /// This is the undocked deconstructor.
+    ///
+    /// 
     ~undocked();
+    /// This is the changestate function.
+    ///
+    ///Used to change the state of the satellite
+    ///@return object of type docked_state
     docked_state* changestate() override;
+     /// this is the getstate function.
+    ///
+    /// This used as an accessor method to private member launched
+    /// @return a bool value
     bool getstate() override;
+    /// This is the clone function.
+    ///
+    ///Used to clone the state of the satellite
+    ///@return object of type docked_state
     docked_state* clone() override;
 };
 #endif // __UNDOCKED_H__
