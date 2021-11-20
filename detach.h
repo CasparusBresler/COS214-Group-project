8a1744e83be87ab2eab438808925ce7f00a23a69
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:38:51 +0200

    Update detach.h

--- detach.h
+++ detach.h
@@ -9,14 +9,38 @@
 
 using namespace std;
 
+/**
+ * @brief new class attach, inherited from the DragonCommand class
+ * Uses the Template method design pattern
+ */
+
 class detach : public DragonCommand
 {
 private:
 public:
-
+    /**
+     * @brief Construct a new detach object
+     * Constructor for the detach class
+     * @return nothing
+     */
     detach();
+    
+     /**
+     * @brief Destroy the detach object
+     * Destructor for the detach class
+     * @return nothing
+     */
     ~detach();
-    void execute(dragon_template* , storage*) override;
+    
+     /**
+     * @brief Overridden execute function of detach class
+     * Subclass in Template method design pattern
+     * @return nothing
+     * @see DragonCommand class
+     * @param dt dt is of type dragon_template*
+     * @param s s is of type storage*
+     */
+    void execute(dragon_template* dt , storage* s) override;
 
 };
 
