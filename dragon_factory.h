0e625e602a9013b71724e905a90089c25610687d
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:43:18 +0200

    Update dragon_factory.h

--- dragon_factory.h
+++ dragon_factory.h
@@ -6,15 +6,42 @@
 #define __DRAGON_FACTORY_H__
 
 #include "dragon_template.h"
+
 using namespace std;
+
+/**
+ * @brief Dragon Factory class 
+ * Uses the Factory method design pattern.
+ */
 
 class dragon_factory
 {
 private:
 public:
+        /**
+     * @brief Construct a new dragon_factory object
+     * This is the dragon_factory constructor function 
+     * @return nothing
+     */
     dragon_factory();
+    
+    /**
+     * @brief Destroy the dragon_factory object
+     * Destructor for dragon_factory class
+     * @return nothing
+     */
     ~dragon_factory();
-    virtual dragon_template *createDragon(ISS*) = 0;
+    
+        /**
+     * @brief Create a Dragon object
+     * virtual function
+     * Uses the Factory design pattern
+     * 
+     * @return dragon_template* in concrete classes
+     * @see dragonCargo_factory
+     * @see dragonCrew_factory
+     */
+    virtual dragon_template *createDragon() = 0;
 };
 
 #endif // __DRAGON_FACTORY_H__
