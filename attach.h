f18556d0398a771ff95fc341321713701e0f0590
DanieldeNoberga <92922089+DanieldeNoberga@users.noreply.github.com>
2021-11-20 21:33:58 +0200

    Update attach.h

--- attach.h
+++ attach.h
@@ -5,13 +5,37 @@
 
 using namespace std;
 
+/**
+ * @brief new class attach, inherited from the DragonCommand class
+ * Uses the Template method design pattern
+ */
+
 class attach : public DragonCommand
 {
 private:
 public:
-
+    /**
+     * @brief Construct a new attach object
+     * Constructor for the attach class
+     * @return nothing
+     */
     attach();
+    
+     /**
+     * @brief Destroy the attach object
+     * Destructor for the attach class
+     * @return nothing
+     */
     ~attach();
+
+     /**
+     * @brief Overridden execute function of attach class
+     * Subclass in Template method design pattern
+     * @return nothing
+     * @see DragonCommand class
+     * @param dt dt is of type dragon_template*
+     * @param s s is of type storage*
+     */
     void execute(dragon_template* , storage*) override;
 
 };
