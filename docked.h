062f43cd6d8251711abb563f8cb7929063ccbf00
CasparusBresler <92540552+CasparusBresler@users.noreply.github.com>
2021-11-20 21:43:01 +0200

    Update docked.h

--- docked.h
+++ docked.h
@@ -8,15 +8,58 @@
 
 using namespace std;
 
+/**
+ * @brief new docked class
+ * Inherited from docked_state
+ * Uses the state design pattern
+ * 
+ */
 class docked : public docked_state
 {
 private:
     bool dock_state;
 public:
+
+/**
+     * @brief Construct a new docked object
+     * Constructor for the docked class
+     * @return nothing 
+     */
     docked(/* args */);
+    
+    
+    /**
+     * @brief Destroy the docked object
+     * Destructor for the docked state
+     * @return nothing
+     */
     ~docked();
+    
+    /**
+     * @brief function to change the state of the docked state
+     * 
+     * @return docked_state* 
+     * @see docked_state class 
+     */
     docked_state* changestate() override;
+    
+    /**
+     * @brief getstate function for the docked class
+     * Function that gets the state and returns it
+     * @see docked_state class
+     * 
+     * @return true 
+     * @return false 
+     */
     bool getstate() override;
+    
+    /**
+     * @brief clone function for the docked class
+     * Uses the Prototype design pattern
+     * 
+     * @see docked_state class
+     * @return docked_state* 
+     */
     docked_state* clone() override;
 };
 #endif // __DOCKED_H__
